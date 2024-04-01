91829bd53dee0619573bfbe110fac51461739e71
yangshoong <crinaeae83@gmail.com>
2024-04-02 01:26:44 +0900

    submit

--- src/component/ContactItem.js
+++ src/component/ContactItem.js
@@ -5,7 +5,7 @@ const ContactItem = ({ item }) => {
   return (
     <Row className='contact-item'>
       <Col lg={1}>
-        <img width={50} src="https://st4.depositphotos.com/7752738/22685/v/450/depositphotos_226857182-stock-illustration-user-icon-vector-line-outline.jpg" />
+        <img width={50} src="https://st4.depositphotos.com/7752738/22685/v/450/depositphotos_226857182-stock-illustration-user-icon-vector-line-outline.jpg" alt='' />
       </Col>
       <Col lg={9}>
         <div className='contact-info'>
